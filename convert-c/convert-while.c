015acd4a0df29cb988a98a7bdc3ea4a5b3f4c0ea
TryaNever <alexispernette@outlook.fr>
2026-08-13 13:50:07 +0200

    rename file

--- convert-c/convert-while.c
+++ convert-c/convert-while.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 
 int main() {
-    int min,max,range;
-    float fahr, celcus;
+    int min,range;
+    long long int max,fahr;
+    float celcus;
 
     min = 0;
-    max = 2000;
-    range = 20;
+    max = 2000000000000000000;
+    range = 1;
 
     fahr = min;
 
     while (fahr <= max)
     {
         celcus = (fahr - 32) * 5/9;
-        printf("%3.0f\t%6.2f\n",fahr,celcus);
+        printf("|%3.0f  |  %6.2f|\n",fahr,celcus);
         fahr = fahr + range;
     }
     system("PAUSE");
