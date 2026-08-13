7ee1c0e62ff28eda2d57920584345e64e2c3ac08
TryaNever <alexispernette@outlook.fr>
2026-08-13 09:28:09 +0200

    add convert.c to implement temperature conversion setup

---
+++ convert-c/convert.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int min,max,range;
+    float fahr, celcus;
+
+    min = 0;
+    max = 2000;
+    range = 20;
+
+    fahr = min;
+
+
+    
+}
