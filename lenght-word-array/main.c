204bec1e007ac8fe30c4b5f34e55b010f7878b71
TryaNever <alexispernette@outlook.fr>
2026-08-15 18:43:09 +0200

    wip

--- lenght-word-array/main.c
+++ lenght-word-array/main.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-    int c;
-    int number_letter, n_length[10];
-
+    int c,i;
+    int number_letter = 0;
+    int n_length[10] = {0};
 
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t')
         {
-            printf("y a un espace ici");
-            n_length[0 - number_letter]++;
-        } else {
-            printf("%d", number_letter);
+            n_length[number_letter - 1]++;
+            printf("new word\n");
+            number_letter = 0;
         }
-        
-        number_letter++;
+        else {
+            printf("new letter\n");
+            number_letter++;
+        }
+    }
+    for(i = 0; i <= sizeof(n_length);i++) {
+        printf("%d\n", n_length[i]);
     }
     system("PAUSE");
 }
