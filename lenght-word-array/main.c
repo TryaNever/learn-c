e4e996f9a8af2d52c831f4a0f550be4b9ea3e817
TryaNever <alexispernette@outlook.fr>
2026-08-16 13:38:06 +0200

    refactor main function and add helper functions for array processing

--- lenght-word-array/main.c
+++ lenght-word-array/main.c
@@ -1,24 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
-int main() {
-    int c,i;
-    int number_letter = 0;
+
+void print_array(int array[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+    }
+    
+}
+
+int higher_num_array(int array[], int size) {
+    int max,i;
+    max = array[0];
+    for (i = 0; i < size; i++) {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    int c, i,row,col;
+    int number_letter = 0, higter_num = 0;
     int n_length[10] = {0};
+    int size_n_length;
+    size_n_length = sizeof(n_length) / sizeof(n_length[0]);
     c = getchar();
-    while (c != EOF) {
-        if (c == '\n')
+    while (c != EOF)
+    {
+        if (c == '\n'){
+            n_length[number_letter - 1]++;
             break;
-        if (c == ' ' || c == '\t') {
+        }
+        else if (c == ' ' || c == '\t')
+        {
             n_length[number_letter - 1]++;
             number_letter = 0;
-        } else 
+        }
+        else
             number_letter++;
         c = getchar();
     }
-    for(i = 0; i < sizeof(n_length) / sizeof(n_length[0]);i++) {
-        printf("%d\n", n_length[i]);
-    }
+    higter_num = higher_num_array(n_length,size_n_length);
     
+    for (row = 0; row < size_n_length; row++) {
+        for (col = 0; col < higter_num; col++)
+        {
+            if (n_length[row] > col)
+            {
+                printf("▮");
+            } else {
+                printf("▯");
+            }
+        }
+        printf("\n");  
+    }
     system("PAUSE");
 }
