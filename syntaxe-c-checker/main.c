5d76cd550fe7599180662930403934791ecf55e8
TryaNever <alexispernette@outlook.fr>
2026-08-18 22:48:59 +0200

    refactor main function to improve character detection and handling

--- syntaxe-c-checker/main.c
+++ syntaxe-c-checker/main.c
@@ -1,28 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAXCRT 100000
+#define MAXCRT 200
 
 void print_array(int array[], int size);
 
 int main()
 {
-    int c, i, size_array;
+    int c, nb_crt_detected, size_array, i;
     int lign[MAXCRT] = {0};
 
-    i = 0;
+    nb_crt_detected = 0;
 
     while ((c = getchar()) != EOF)
     {
         if (c == '{' || c == '}' || c == '\"')
         {
-            lign[i] = c;
-            i++;
+            lign[nb_crt_detected] = c;
+            nb_crt_detected++;
         }
     }
 
+    int out, in;
+    int in_paren = 0;
 
-    print_array(lign, i);
+    out = in = 0;
+    for (i = 0; i < nb_crt_detected; i++)
+    {
+        if (lign[i] == '{' && in_paren == 0)
+        {
+            in++;
+        }
+        else if (lign[i] == '}' && in_paren == 0)
+        {
+            out++;
+        }
+        else if (lign[i] == '\"' && in_paren == 0)
+        {
+            in++;
+            in_paren = 1;
+        }
+        else if (lign[i] == '\"' && in_paren == 1)
+        {
+            out++;
+            in_paren = 0;
+        }
+        else
+        {
+            printf("error in \"\" you miss close i think ;)");
+        }
+    }
+    if (in != out || in_paren == 1)
+    {   
+        printf("you miss close your {} ;)");
+    }
+    else
+    {
+        printf("nothing syntaxe error detected");
+    }
+
+    print_array(lign, nb_crt_detected);
     system("PAUSE");
     return 0;
 }
