39d88eecc3b0075903d72915675f105508c0fc20
TryaNever <alexispernette@outlook.fr>
2026-08-18 22:25:12 +0200

    refactor main function to improve structure and fix character handling

--- syntaxe-c-checker/main.c
+++ syntaxe-c-checker/main.c
@@ -3,20 +3,35 @@
 
 #define MAXCRT 100000
 
-int main() {
-    int c,i;
-    int lign[] = 0;
+void print_array(int array[], int size);
+
+int main()
+{
+    int c, i, size_array;
+    int lign[MAXCRT] = {0};
 
     i = 0;
 
-    while ((c = getchar()) != EOF) {
-        if (c == "{" || c == "}" || c == "\"")
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '{' || c == '}' || c == '\"')
         {
             lign[i] = c;
             i++;
         }
-
     }
 
+
+    print_array(lign, i);
+    system("PAUSE");
     return 0;
+}
+
+void print_array(int array[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%c", array[i]);
+    }
 }
