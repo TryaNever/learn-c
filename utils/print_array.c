05267021709c114067e25091f53c072a85a72544
TryaNever <alexispernette@outlook.fr>
2026-08-24 00:15:01 +0200

    refactor print_array function for improved formatting and readability

--- utils/print_array.c
+++ utils/print_array.c
@@ -1,30 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_array(int arr[],size_t count) 
-    {
+void print_array(int arr[], size_t count)
+{
     printf("[\n");
     for (size_t i = 0; i < count; i++)
     {
         printf(" %d", arr[i]);
-        if (i % 5 == 0)
+        if (i != count - 1)
         {
-            printf(",");
+            if (i % 5 == 4 || i == count - 1)
+                printf(",\n");
+            else
+                printf(", ");
         }
-        else if (i != count - 1)
-        {
-            printf(",\n");
-        }
-        
-        
+        else
+            printf("\n");
     }
     printf("]\n");
-
-    
 }
 
-int main() {
-    int arr[] = {10,20,40,20};
+int main()
+{
+    int arr[] = {10, 20, 40, 20, 50, 80, 50};
 
     size_t count = sizeof(arr) / sizeof(arr[0]);
 
