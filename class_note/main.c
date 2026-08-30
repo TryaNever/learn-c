fe08a624ba4e2088a06e630e7c713d8e915a6f73
TryaNever <alexispernette@outlook.fr>
2026-08-31 01:08:04 +0200

    refactor main function to improve clarity and separate note printing logic

--- class_note/main.c
+++ class_note/main.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 int get_note(int student_number);
+void print_note(int *notes, int size);
 
 int main()
 {
-    int many_people;
+    int many_people, size_note;
 
     printf("Choose who many people in class room (2 at 30) :");
 
     scanf("%d", &many_people);
+    size_note = many_people;
 
     if (many_people < 2 || many_people > 30)
     {
@@ -21,13 +23,11 @@ int main()
 
     for (int i = 0; i < many_people; i++)
     {
-        class_note[i] = get_note(i + 1);
+        class_note[i] = get_note(i);
     }
 
-    for (int i = 0; i < many_people; i++)
-    {
-        printf("Eleve %d: %d \n", i + 1, class_note[i]);
-    }
+    print_note(class_note, size_note);
+
     return 0;
 }
 
@@ -48,4 +48,12 @@ int get_note(int student_number)
             printf("The note are Valid\n");
         }
     } while (note > 20 || note < 0);
+}
+
+void print_note(int *notes, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Eleve %d: %d \n", i + 1, notes[i]);
+    }
 }
